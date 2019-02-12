75ab89568b44832e04e2d5d4c62d53a20d51a1ed
gkntnm <gkntnm@gmail.com>
2019-02-12 14:05:34 +0400

    Добавлен класс для хранения токена после авторизации.

---
+++ app/src/main/java/com/example/trainapp1fitsmeclone/Domain/Entities/AuthInfo.java
@@ -0,0 +1,14 @@
+package com.example.trainapp1fitsmeclone.Domain.Entities;
+
+public class AuthInfo {
+    private String login;
+    private String token;
+
+    public  AuthInfo (String login, String token){
+        this.login = login;
+        this.token = token;
+    }
+
+    public String getLogin(){ return login;}
+    public String getToken(){ return token;}
+}
