3bbce53a94498a4cc2c7c7f74499b318028463ec
gkntnm <gkntnm@gmail.com>
2019-02-12 13:57:23 +0400

    Добавлен класс для входа в систему.

---
+++ app/src/main/java/com/example/trainapp1fitsmeclone/Domain/Entities/SignInInfo.java
@@ -0,0 +1,21 @@
+package com.example.trainapp1fitsmeclone.Domain.Entities;
+
+public class SignInInfo {
+    private String login;
+    private String passwordHash;
+
+    public SignInInfo(String login, String password){
+        this.login = login;
+        this.passwordHash = convertToSha256(password);
+    }
+
+    private String convertToSha256(String password) {
+        return password+ "_temp_hash";
+    }
+
+    public String getLogin() {return login;}
+
+    public String getPasswordHash() {return passwordHash;}
+
+
+}
