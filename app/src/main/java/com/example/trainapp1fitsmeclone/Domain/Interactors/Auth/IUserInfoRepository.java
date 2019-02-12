39dac094d7a8ca79fd0f67b0b218f0469385bd4f
gkntnm <gkntnm@gmail.com>
2019-02-12 17:35:00 +0400

    temp commit

---
+++ app/src/main/java/com/example/trainapp1fitsmeclone/Domain/Interactors/Auth/IUserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.example.trainapp1fitsmeclone.Domain.Interactors.Auth;
+
+import android.support.annotation.NonNull;
+
+import com.example.trainapp1fitsmeclone.Domain.Entities.SignInInfo;
+
+interface IUserInfoRepository {
+
+    @NonNull
+    SignInInfo get
+}
