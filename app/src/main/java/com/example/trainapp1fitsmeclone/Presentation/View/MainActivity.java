b70bc76c391e8f6878422eb5c1f6ff0f9e41e278
gkntnm <gkntnm@gmail.com>
2019-02-13 15:12:19 +0400

    MainActivity перемещена в Presentation.View

--- app/src/main/java/com/example/trainapp1fitsmeclone/Presentation/View/MainActivity.java
+++ app/src/main/java/com/example/trainapp1fitsmeclone/Presentation/View/MainActivity.java
@@ -1,9 +1,12 @@
-package com.example.trainapp1fitsmeclone;
+package com.example.trainapp1fitsmeclone.Presentation.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.trainapp1fitsmeclone.R;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
