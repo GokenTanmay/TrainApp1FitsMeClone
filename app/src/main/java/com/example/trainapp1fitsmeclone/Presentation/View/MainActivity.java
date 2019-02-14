29bf2a2b7066f5e1d75e84d5c05a8843de4fd384
gkntnm <gkntnm@gmail.com>
2019-02-14 13:32:54 +0400

    MainActivity изменен для работы с LifeCycle

--- app/src/main/java/com/example/trainapp1fitsmeclone/Presentation/View/MainActivity.java
+++ app/src/main/java/com/example/trainapp1fitsmeclone/Presentation/View/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.trainapp1fitsmeclone.Presentation.View;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.trainapp1fitsmeclone.App;
 import com.example.trainapp1fitsmeclone.Domain.Interactors.Auth.SignInUpInteractor;
+import com.example.trainapp1fitsmeclone.Presentation.presenter.MainPresenter;
 import com.example.trainapp1fitsmeclone.R;
 
 import javax.inject.Inject;
@@ -22,5 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainPresenter mainPresenter = ViewModelProviders.of(this).get(MainPresenter.class);
     }
 }
---
+++ app/src/main/java/com/example/trainapp1fitsmeclone/Presentation/presenter/MainPresenter.java
@@ -0,0 +1,6 @@
+package com.example.trainapp1fitsmeclone.Presentation.presenter;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainPresenter extends ViewModel {
+}
